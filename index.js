7763378a2ebe8107344fcb10fb05d5c78e9e16ec
EnmanuelJS <galvanenmanuel0@gmail.com>
2025-06-11 21:56:14 -0400

    agregando funcion de visualizacion de stacks

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ class stack {
     }
 
     set insertStackA(value) {
-            this.stackA.push(value);
-            this.stackB.pop(value);
+        this.stackA.push(value);
+        this.stackB.pop(value);
     }
 
     set insertStackB(value) {
@@ -28,35 +28,45 @@ let outputStackA = document.getElementById('textarea_stack_A');
 
 // textarea pila B
 let outputStackB = document.getElementById('textarea_stack_B');
+
 let searchWord = /\s*[A-Za-z0-9]+\b/gm;
 
 //botones
 const buttonA = document.getElementById('button_A');
 const buttonB = document.getElementById('button_B');
 
-buttonA.addEventListener("click", () => {
+const viewStacks = ()=> {// actualizar textareas de stacks con cada cambio 
+    let viewStackA = Stack.stackA.map((element, index) => `${index}:${element}`);
+    let viewStackB = Stack.stackB.map((element, index) => `${index}:${element}`);
+    outputStackA.value = `PILA A:
+[
+    ${viewStackA}
+]`;
 
+    outputStackB.value = `PILA A:
+[
+    ${viewStackB}
+]`;
+
+}
+buttonA.addEventListener("click", () => {
     let filteredWord = input.value.match(searchWord);
-    let lastElementStackB = Stack.stackB[Stack.stackB.length -1]
-    
+    let lastElementStackB = Stack.stackB[Stack.stackB.length - 1]
+
     if (Stack.stackA.length === 0 && Stack.stackB.length === 0) {
         for (let i = 0; i < filteredWord.length; i++) {
             Stack.insertStackA = filteredWord[i];
         }
         input.value = "";
-    }else if(Stack.stackB.length > 0){
+    } else if (Stack.stackB.length > 0) {
         Stack.insertStackA = lastElementStackB;
     }
 
-    outputStackA.value = Stack.stackA;
-    outputStackB.value = Stack.stackB;
-    console.log("StacK A ", Stack.stackA);
+    viewStacks();
 });
 
 buttonB.addEventListener("click", () => {
-    let lastElementStackA = Stack.stackA[Stack.stackA.length - 1]; 
-
+    let lastElementStackA = Stack.stackA[Stack.stackA.length - 1];
     Stack.insertStackB = lastElementStackA;
-    outputStackB.value = Stack.stackB;
-    outputStackA.value = Stack.stackA;
+    viewStacks();
 });
