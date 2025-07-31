3140ceb06041a80e8d4295e821d70903b19cc42b
EnmanuelJS <galvanenmanuel0@gmail.com>
2025-07-30 22:44:33 -0400

    agregando elementos a dialog

--- src/array.js
+++ src/array.js
@@ -1,14 +1,78 @@
-class array {//tener en cuenta que este array es dinamico
-    constructor(){
-        this.array = [];
+class array {//tener en cuenta que este array es dinamico y el metodo de eliminacion de cada elemento se descartó
+    constructor() {
+        this.arrayA = [];
+        this.arrayB = [];
     }
 
-    set addElement(element){
-        this.array.push(element);
-    } 
+    addElementArrayA(index, element) {
+        if (this.arrayA.length === 0) {
+            this.arrayA.push(element);
 
-    getElement(index){
-        return this.array[index];
+        } else {
+            this.arrayA.splice(index, 1, element);
+            this.arrayB.pop(element);
+        }
+    }
+
+    addElementArrayB(index, element) {
+        if (this.arrayA.length <= 0) {
+            return;
+        } else {
+            this.arrayB.splice(index, 1, element);
+            this.arrayA.pop(element);
+
+        }
+    }
+
+    deleteAllElements() {
+        this.arrayA = [];
+        this.arrayB = [];
+    }
+
+    getElementArrayA(index) {
+        return this.arrayA[index];
+    }
+
+    getElementArrayB(index) {
+        return this.arrayB[index];
+    }
+
+}
+
+let Array = new array();
+let outputA = document.getElementById('output-A');
+let outputB = document.getElementById('output-B');
+let dialogElements = document.querySelector("dialog");
+let searchWord = /[A-Za-z0-9]+\b/gm;
+let filteredWord;
+
+
+export const viewElements = () => {
+    let viewArrayA = Array.arrayA.map((element, index) => `<strong>${index}:</strong>${element} `);
+    let viewArrayB = Array.arrayB.map((element, index) => `<strong>${index}:</strong>${element} `);
+    outputA.innerHTML = `CONTENEDOR A:
+[
+    ${viewArrayA}
+]`;
+
+    outputB.innerHTML = `CONTENEDOR B:
+[
+    ${viewArrayB}
+]`;
+}
+
+export const actionInput = (input) => {
+    Array.deleteAllElements();
+    filteredWord = input.match(searchWord);
+    viewElements();
+};
+
+export const actionButtonA = (input) => {
+    if (Array.arrayA.length === 0 && Array.arrayB.length === 0) {
+        Array.addElementArrayA = filteredWord;
+        input.value = "";
+    } else {
+        dialogElements.showModal();
     }
 
 }
