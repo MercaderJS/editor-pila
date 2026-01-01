3e5e45d79d066643928e3b3438a174e7980131d8
MercaderJS <enmanuelsegura859@gmail.com>
2026-01-01 16:36:31 -0400

    insertando elementos de textarea desde button_A

--- src/array.js
+++ src/array.js
@@ -1,6 +1,6 @@
 class array {//tener en cuenta que este array es dinamico y el metodo de eliminacion de cada elemento se descartó
     constructor() {
-        this.arrayA = [1,2,3];
+        this.arrayA = [];
         this.arrayB = [];
     }
 
@@ -32,7 +32,7 @@ class array {//tener en cuenta que este array es dinamico y el metodo de elimina
         this.arrayA = [];
         this.arrayB = [];
     }
-
+// Aun no se para que carajo los voy a usar
     getElementArrayA(index) {
         return this.arrayA[index];
     }
@@ -61,10 +61,10 @@ let elementsDialog;
 let indexElementsDialog;
 let isDialogArrayA;
 
-let input = document.getElementById("textarea_write");
+// let input = document.getElementById("textarea_write");
 
 
-const viewElements = () => {
+const viewData = () => {
     let viewArrayA = Array.arrayA.map((element, index) => `<strong>${index}:</strong>${element} `);
     let viewArrayB = Array.arrayB.map((element, index) => `<strong>${index}:</strong>${element} `);
     outputA.innerHTML = `CONTENEDOR A:
@@ -78,6 +78,12 @@ const viewElements = () => {
 ]`;
 }
 
+
+const actionInput = (input) => {
+    Array.deleteAllElements();
+    filteredWord = input.value.match(searchWord);
+};
+
 const viewElementsDialog = (arr) => {
     return arr.map((element, index) => `<li id="element-dialog" value="${element}">${element}</li>`);
 }
@@ -86,24 +92,16 @@ const viewIndexDialog = (arr) => {
     return arr.map((element, index) => `<li id="index-element-dialog" value="${index}">${index}</li>`);
 }
 
-const actionInput = (index,input) => {
-    Array.deleteAllElements();
-    filteredWord = input.match(searchWord);
-        Array.addElementArrayA(index,filteredWord);
-        
-    viewElements();
-};
-
 const getDataDialog = (index,element)=> {// ==========Averiguar porque se ejecuta mas de una vez tras cada llamada=========
     isDialogArrayA ? Array.addElementArrayA(index,element) : Array.addElementArrayB(index,element);
-    viewElements();
-    console.log(isDialogArrayA);
-    console.log(index,element);
-    
-    
 }
 
 const actionButtonA = (input) => {
+    if (input.value != "") {
+        Array.addElementArrayA(indexesDialogSelected,filteredWord);
+        
+    } else {
+        
     isDialogArrayA = true;
     dialog = document.querySelector("dialog");
     buttonDialog = document.getElementById("button-dialog");
@@ -113,12 +111,6 @@ const actionButtonA = (input) => {
     elementsDialog = document.querySelectorAll("#element-dialog");
     indexElementsDialog = document.querySelectorAll("#index-element-dialog");
 
-    if (input.value !== "") {
-        actionInput(indexesDialogSelected,input.value);
-        input.value = "";
-        
-    } else {
-        
     dialog.showModal();
     elementsDialog.forEach((element, index)=> element.addEventListener("click",()=> {
         indexesDialogSelected.splice(0, 1, index);
@@ -132,8 +124,7 @@ const actionButtonA = (input) => {
     }));
 
     buttonDialog.addEventListener("click", ()=> {
-        getDataDialog(indexesDialogSelected, elementDialogSelected)
-        viewElements()
+        getDataDialog(indexesDialogSelected, elementDialogSelected);
         elementDialogSelected = null;
         indexesDialogSelected = [];
         isDialogArrayA = false;
@@ -141,11 +132,11 @@ const actionButtonA = (input) => {
         return;
     })
 }
-
 }
 
 
-document.getElementById("button_B").addEventListener("click",()=>{
+
+const actionButtonB = ()=> {
     isDialogArrayA = false;
     dialog = document.querySelector("dialog");
     buttonDialog = document.getElementById("button-dialog");
@@ -169,15 +160,33 @@ document.getElementById("button_B").addEventListener("click",()=>{
 
     buttonDialog.addEventListener("click", ()=> {
         getDataDialog(indexesDialogSelected, elementDialogSelected);
-        viewElements()
         elementDialogSelected = null;
         indexesDialogSelected = [];
         isDialogArrayA = true;
         dialog.close(); 
         return;
-    })
+    });
+    
 }
-);
 
-document.getElementById("button_A").addEventListener("click", ()=> actionButtonA(input))
-viewElements()
+const arraysEmpty = ()=> {
+    Array.arrayA.length === 0 &&  Array.arrayB.length === 0 ?  true : false;
+}
+
+const arrayAfull = ()=> {
+    Array.arrayA.length > 0 && Array.arrayB.length === 0 ? true : false;
+}
+
+const arrayBfull = ()=> {
+    Array.arrayB.length > 0 && Array.arrayA.length === 0 ? true : false;
+}
+
+export const structureArray = {
+    "viewData": viewData,
+    "actionInput": actionInput,
+    "actionButtonA": actionButtonA,
+    "actionButtonB": actionButtonB,
+    "stateButtons" : arraysEmpty,
+    "stateButtonA":  arrayAfull,
+    "stateButtonB":  arrayBfull
+}
